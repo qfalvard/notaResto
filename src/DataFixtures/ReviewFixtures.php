fd72ca6f9129ad01d85a3a1a369a639c2e7f08a5
qfalvard <qufalvard@gmail.com>
2020-03-17 11:57:18 +0100

    city review fixtures to begin

--- src/DataFixtures/ReviewFixtures.php
+++ src/DataFixtures/ReviewFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class CityFixtures extends Fixture
+class ReviewFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
