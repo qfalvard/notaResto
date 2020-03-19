906b91fae10734425700bdbe867263e879f08f13
qfalvard <qufalvard@gmail.com>
2020-03-19 14:21:37 +0100

    finish userFixtures test

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,56 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Repository\CityRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements DependentFixtureInterface
 {
+    private $encoder;
+    private $cityRepository;
+
+    public function __construct(UserPasswordEncoderInterface $encoder, CityRepository $cityRepository)
+    {
+
+        $this->encoder = $encoder;
+        $this->cityRepository = $cityRepository;
+    }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $randomCity = $this->cityRepository->find(rand(1,1000));
+
+        $userAdmin = new User();
+        $userAdmin->setEmail('moderateur@notaresto.com');
+        $userAdmin->setPassword($this->encoder->encodePassword($userAdmin, 'notaresto'));
+        $userAdmin->setRoles(['ROLE_ADMIN']);
+        $userAdmin->setCity($randomCity);
+        $manager->persist($userAdmin);
+
+        $userClient = new User();
+        $userClient->setEmail('client@notaresto.com');
+        $userClient->setPassword($this->encoder->encodePassword($userClient, 'notaresto'));
+        $userClient->setRoles(['ROLE_CLIENT']);
+        $userClient->setCity($randomCity);
+        $manager->persist($userClient);
+
+        $userRestaurateur = new User();
+        $userRestaurateur->setEmail('restaurateur@notaresto.com');
+        $userRestaurateur->setPassword($this->encoder->encodePassword($userRestaurateur, 'notaresto'));
+        $userRestaurateur->setRoles(['ROLE_RESTO']);
+        $userRestaurateur->setCity($randomCity);
+        $manager->persist($userRestaurateur);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            CityFixtures::class,
+        );
     }
 }
